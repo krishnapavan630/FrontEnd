0b9edab5bdf3ff47298a5c3059702dadf56c8f5a
krishnapavan630 <77038420+krishnapavan630@users.noreply.github.com>
2025-11-09 21:50:31 +0530

    Add files via upload

---
+++ Day12/script.js
@@ -0,0 +1,66 @@
+
+// var Myname='krishna';
+
+// console.log('My name is',Myname);
+
+// My name is krishna
+
+// suvidha
+// 7
+
+'My name is suvidha my favorite numer is 7'
+
+// Variables,datatypes
+
+// Variable
+
+// 1)var  -- fuction scoped
+// 2)let  -- block scoped
+// 3)const
+
+// 1) Declaraation  --> var Myname
+// 2) Assign --> Myname = 'Suvidha'
+
+
+// var Myname;
+
+// Myname = 'Suvidha';
+
+// console.log(Myname);
+
+// var Myname = 'suvidha';
+
+// console.log(Myname);
+
+// if (true){
+//     var age=20;
+// }
+
+// console.log(age)
+
+
+// if (true){
+//     let num=10;
+//     console.log(num);
+// }
+
+// console.log(num);
+
+// var Myname='krishna';
+
+// var Myname='suvidha';
+
+// var INr=88.64;
+
+// let lastname='Mandarpu';
+
+// let lastname='Karipetti';
+
+// const pi=3.14;
+
+// console.log(lastname);
+
+// console.log('This is my first js code');
+
+
+
---
+++ Day13/script.js
@@ -0,0 +1,89 @@
+// Variables and datatypes
+
+// var
+// let
+// const
+
+// --> camel casing
+
+// var -- Function Scoped(redeclare, reassign)
+
+// let -- Block Scoped (redeclaration is not accepted, reassigning )
+
+// const -- Block Scoped (redeclaration is not accepted, reassigning is not possible)
+
+// 1)declare 
+// 2)assigning
+
+// var myname;
+
+// myname = 'suvidha';
+
+// var myname='krishna';
+
+// console.log(myname);
+
+// let myName='suvidha';
+
+// let lastName='Mandarapu';
+
+// myName='krishna';
+
+// console.log(myName);
+
+// const pi=3.14;
+
+// pi=2.14;
+
+// console.log(pi);
+
+// Datatypes
+// 1) Primitive Datatypes -- 1 variable 1 value
+// 2) NonPrimitive Datatypes -- 1 variable multiple values
+
+// 1) Primitive Datatypes:
+
+// Numbers
+// strings
+// Booleans True or False
+// undefined
+// null
+// BigInt
+// symbols immutable(cannot be chaged)
+
+
+// let sym = Symbol('hello') ;
+
+// console.log(sym);
+
+// 2)NonPrimitive Datatypes -- 1 variable multiple values
+
+// 1)Object   key:value
+// 2)Array
+// 3)function
+
+// let obj={
+// name:'Suvidha',
+// phno:7799,
+// bg:'b+ve'
+// };
+
+// console.log(obj);
+
+// 2) Array
+
+// let students = ["krishna","pavan","ajay","suvidha","pinky"];
+
+// console.log(students);
+
+// let name='suvidha';
+
+// let age=17;
+
+// console.log("My name is",name,"my age is",age);
+
+// console.log(`My name is ${name} and my age is ${age}`);
+
+// friends=[frnd1,frnd2,frnd3]
+
+// My name is suvidha my age is 17 and among my friends my best friend is frnd2
