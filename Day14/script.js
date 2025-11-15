5fa016324a4fd832ba86ee1ec08b4e1309266ba6
krishnapavan630 <77038420+krishnapavan630@users.noreply.github.com>
2025-11-16 00:03:06 +0530

    Add files via upload

---
+++ Day14/script.js
@@ -0,0 +1,55 @@
+//Operators
+// 1) Arthimetic operators (+,-,*,/,%,**,++,--)
+// 2) Assignemt operators (=,+=,-=,*=,/=,**=)
+// 3) Comparission operators(==,===,<,>,<=,>=,!=)
+// 4) Logical operators (and,or, not)
+
+
+// a=5
+
+// console.log("value of a in post incrementation ",a++);
+// console.log("the value of after incrementation is", a);
+// console.log("the value of after incrementation is", a);
+
+// a=5
+
+// console.log("value of a in pre incrementation ",++a);
+
+// a=5
+// console.log("value of a in post decrement ",a--);
+// console.log("the value of after decrement is", a);
+
+// a=5
+// console.log("value of a in post decrement ",--a);
+
+// a=10
+// console.log(a++ + ++a)
+
+// a=10
+// a-=2  // a=a-2  -- > 10-2=8
+// console.log(a)
+
+// == Lose comparision --> It cheks only value not type
+
+// a=5
+// b='5'
+// console.log(a==b)
+
+// === tight comparision --> It cheks value and type
+
+// a=5
+// b='5'
+// console.log(a===b)
+
+// a=7
+// b=5
+
+// // console.log(a<=b)
+// console.log(a !== b)
+
+// a=10
+// b=5
+
+// // console.log(a>b || a<b)
+
+// console.log(!(a<b))
