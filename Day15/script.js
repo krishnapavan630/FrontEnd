56c9e33a5f505e38b30de6d9c17e6f6840c966d4
krishnapavan630 <77038420+krishnapavan630@users.noreply.github.com>
2025-11-16 23:38:57 +0530

    Add files via upload

---
+++ Day15/script.js
@@ -0,0 +1,146 @@
+// Operator Precedenci
+
+// 1+2*3+16
+// 1+6+16
+// 23
+
+// 1) Paranthesis ()
+// 2) exponents **  
+// 3) multiplication& division & modulos
+// 4) addition & subtraction
+
+// let result = 1 + 2 * 3 + 4 ** 2
+// (m+p+c)/3
+// console.log(result)
+
+// controll statements
+
+// 1) Conditional statements (simple if, if else, else if)
+// 2) Loops (for, while ,do while)
+
+
+// simple if
+
+// syntax: 
+// //  if (cond){
+// //     statement
+// //  }
+
+// let age=18;
+
+// if(age>18){
+//     console.log("You are eligible for voting");
+// }
+// else{
+//     console.log("You are not eligible for voting");
+// }
+
+// syntax:
+// if-else
+
+// if (cond){
+//     statement1
+// }else{
+//     statement2
+// }
+
+// let age=18;
+
+// if(age>18){
+//     console.log("You are eligible for voting");
+// }
+// else{
+//     console.log("You are not eligible for voting");
+// }
+
+// syntax:else-if
+
+// if (cond1){
+//     statement1
+// }
+// else if(cond2){
+//     stament2
+// }
+// else if(cond3){
+//     stament3
+// }
+// .
+// .
+// .
+// .
+// else{
+//     statement4
+// }
+
+// let age=18;
+
+// if (age>18)
+
+// let age=18;
+
+// if(age>18){
+//     console.log("You are eligible for voting")
+// }else if(age===18){
+//     console.log("You have to wait for 1 year")
+// }else{
+//     console.log("You are not eligible")
+// }
+
+// let val=6
+
+// if(val%2===0){
+//     console.log("even")
+// }else{
+//     console.log("odd")
+// }
+// output
+// the value 6 is even
+
+// let val=6
+
+// if(val%2===0){
+//     console.log("even")
+// }else{
+//     console.log("odd")
+// }
+
+// terinary operator
+
+// syntax:    
+            // cond?true output:false output
+
+// let age=19;
+
+// result= age>18?"Major":"Minor"
+
+// console.log(result)
+
+
+// DSA : efficient
+// Time complexity
+// Space complexity 
+
+Q1) take 2 numbers from user print which is a bigger number if both numbers are same print both numbers are same?
+senario1:
+7,8 - input 
+8 is bigger than 7 - output 
+senario2:
+7,7 -input
+output : both numbers are same
+
+Q2)Implement all the discussed emaples using terinary operator
+
+Q3)The price of a violine is $450 due to black friday sale there is a 67% off on the products, what will be the price of the violine
+
+Q4) collect maths, physics and chemistry marks and find the average, 
+If the average is above 90 the grade is A+
+If the average is above 80 the grade is A
+If the average is above 70 the grade is B
+If the average is above 60 the grade is C
+If the average is below 60 then grade is Fail
+
+inputs -- 90,80,85
+
+code snippet : avg=87
+
+output: the student avg is 87 and his grade is A
