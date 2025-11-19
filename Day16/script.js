10e86e2e793dc1fd00787e40b1acc8cbf9b9ea7e
krishnapavan630 <77038420+krishnapavan630@users.noreply.github.com>
2025-11-19 08:20:02 +0530

    Add files via upload

---
+++ Day16/script.js
@@ -0,0 +1,52 @@
+// Loops
+
+// 1)for
+// 2)while
+// 3)do-while
+
+// I love JS   100 times print
+
+// // let m=90;
+
+
+// for loop
+
+// syntax: 
+//             for(intialization;condition;increment){
+//                 //statement
+//             }
+
+// iterate or itteration
+
+// for(let i=0;i<5;i++){
+//     console.log("I love JS")
+// }
+
+// for(let i=1;i<=5;i++){
+//     console.log(i)
+// }
+
+
+
+// for(let i=0;i<=5;i++){
+//     console.log(i);
+// }
+
+// for(let i=0;i<=10;i++){
+//     console.log(`i value is ${i}`)
+// }
+
+// strings and arrays we can use for loop for-of
+
+// let name='Suvidha'
+
+// for(let i of name){
+//     console.log(i)
+// }
+
+// let students=["suvidha","krishna","ajay"]
+
+// for(let name of students){
+//     console.log(name)
+// }
+
