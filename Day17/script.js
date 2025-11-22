2dbf9848bb9a753bde7b361c21c18e91ae07fd49
krishnapavan630 <77038420+krishnapavan630@users.noreply.github.com>
2025-11-23 00:23:35 +0530

    Add files via upload

---
+++ Day17/script.js
@@ -0,0 +1,97 @@
+// let obj={name:"Suvi",empid:234,city:"Pheonix"}
+
+// for(let i in obj){
+//     console.log(obj[i])
+// }
+
+// console.log(obj["name"])
+// // console.log(obj.empid)
+// // console.log(obj)
+
+// q1)What is the sum of 1 to 20 numbers
+
+// let sum=0
+
+// for(let i=1;i<=5;i++){
+//     sum=sum+i
+//     console.log(sum)
+// }
+
+// for(let i=1;i<=5;i++){
+//     sum=sum+i
+// }
+// console.log(sum)
+
+// While (condition){
+//     //statement
+// }
+
+// for(let i=1;i<=10;i++){
+//     console.log(i)
+// }
+
+// let i=1
+
+// while(i<=10){
+//     console.log(i)
+//     i++
+// }
+
+
+// I Love JavaScript 10 TimeRanges
+
+// for(let i=0;i<10;i++){
+//     console.log("I Love Javascript")
+// }
+
+// let i=0;
+
+// while(i>10){
+//     console.log("I Love Javascript")   
+//     i++
+// }
+
+
+// do {
+//     //statement
+// }while(condition);
+
+// let i=0
+
+// do{
+//     console.log("I Love JavaScript")
+//     i++
+// }while(i<10);
+
+// let gameNum=7;
+
+// let userVal
+
+// let gameNum=7
+// let userVAlue=prompt("Guess the number")
+// while(userVAlue!= gameNum){
+//     userVAlue=prompt("Wrong Guess, Guess again")
+// }
+// console.log("Congratulation!")
+
+q1)
+
+user 2 
+
+2 X 1 = 2
+2 X 2 = 4
+2 X 3 = 6
+.
+.
+.
+.
+2 X 10 = 20
+
+Q2) 
+Print all the even number s between 1 to 100
+
+Q3)
+Add all the even numbers between 1 to 100
+
+Q4)
+Multiply all number from 1 to 10
