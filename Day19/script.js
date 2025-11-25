f1aad1ed269ad4b5e1be9f7e4773254ca8f9a6d7
krishnapavan630 <77038420+krishnapavan630@users.noreply.github.com>
2025-11-25 10:07:03 +0530

    Add files via upload

---
+++ Day19/script.js
@@ -0,0 +1,98 @@
+// let name="Krishna Pavan"
+
+// let a=name.slice(3,7)
+
+// console.log(a)
+
+// let str="Java Script"
+
+// val1 = str.toUpperCase()
+// val2 = str.toLowerCase()
+
+// console.log(val1)
+// console.log(val2)
+
+// [Ajay,krishna,suvidha,pinky]   aeiou
+
+// trim: we are trimming white spaces
+
+// let name="suvidha              "
+
+// console.log(name.length)
+
+// val=name.trim()
+
+// console.log(val.length)
+
+// replace(oldval,newval)
+
+// let name="zero"
+
+// console.log(name.replace("z","h")) //returns value but will not modify the original value
+// newName=name.replace("z","h")
+// console.log(name)
+
+// 1.3.4
+
+// Major.minor.patch(bugs fix)
+
+// 1.3.7
+
+// 1.7.9
+
+// 2.0.5
+
+// let name="pavan"
+// console.log(name.charAt(2))
+
+// Template litteral
+
+// name= "krishna"
+
+// console.log("My name is",name)
+// console.log(`My name is ${name}`) // This is template litteral format
+
+// My name is krishna
+// My friend name is ajay
+
+// escape charecters
+
+// \n \t
+
+// console.log("My name is krishna\nMy friend name is Ajay")
+
+// console.log("My name is krishna\tMy friend name is Ajay")
+// 1tab = 4 spaces
+
+// array
+
+// let arr=["Krishna","Pavan","Siva","Rama"]
+
+// array methods
+
+// let arr=["Krishna",560045,"Banglore"]
+
+// arr[0]="Suvidha"
+// console.log(arr)
+
+// for (let val of arr){
+//     console.log(val)
+// }
+
+// devops()  
+
+// let arr=["Krishna",560045,"Banglore"]
+
+// let len=arr.length
+
+// for (let i=0;i<=len;i++){
+//     console.log(arr[i])
+// }
+
+// q1)[82,50,67,90,95] Find the average of these marks
+
+// 82+50+67+90+95
+// -------------- 
+//       5
+// q2) [250,645,300,900,50] find the discounted price after a discount of 20% eache
+//     [200,516,240,720,40]
