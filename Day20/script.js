b9a08e635f481110f98606f38043a930fbb624d7
krishnapavan630 <77038420+krishnapavan630@users.noreply.github.com>
2025-11-29 10:43:19 +0530

    Add files via upload

---
+++ Day20/script.js
@@ -0,0 +1,89 @@
+// Arrays
+
+// let arr=["Suvidha","Ajay","Pinky"]
+
+// replacing a value in array
+// arr[2]="Sam"
+
+// console.log(arr)
+
+//pop  == is used to remove the end value of an aray and also returns it
+
+// let arr=["Suvidha","Ajay","Pinky"]
+// console.log(arr)
+// let val=arr.pop()
+// console.log(`The final array is ${arr}`)
+// console.log(`The removed value from the array is ${val}`)
+
+// push  It will add an element to the end of the array
+
+// let arr=["Suvidha","Ajay","Pinky"]
+
+// arr.push("krishna")
+
+// console.log(arr)
+
+// let arr=["Suvidha","Ajay","Pinky"]
+// let arr2=["Krishna","Pavan"]
+
+// arr3=arr.concat(arr2)
+
+// console.log(arr3)
+
+// shift -- It will delete starting value
+// unshift -- It will add the element at starting
+
+// let arr=["Suvidha","Ajay","Pinky"]
+
+// arr.shift()
+
+// console.log(arr)
+
+// arr.unshift("Krishna")
+
+// console.log(arr)
+
+// let arr=["Suvidha","Ajay","Pinky"]
+
+// arr.reverse()
+
+// console.log(arr)
+
+// slice splice
+
+// let arr=["Suvidha","Ajay","Pinky","Krishna","Sam"]
+
+// let sub_arr = arr.slice(1,3)
+
+// console.log(sub_arr)
+// console.log(arr)
+
+// splice -- This will change the original array if we want to delete add or replace something anywhere in an array
+
+// arr.splice(start index pos,delete No.of elements,the value it should be replace or added)
+
+// let arr=["Suvidha","Ajay","Pinky","Krishna","Sam"]
+// // ["Suvidha","Ajay","Suchi","Pinky","Krishna","Sam"]
+
+// arr.splice(2,0,"Suchi")
+
+// console.log(arr)
+
+// let arr=["Suvidha","Ajay","Pinky","Krishna","Sam"]
+
+// arr.splice(2,1,"Suchi")
+
+// console.log(arr)
+
+// let arr=["Suvidha","Ajay","Pinky","Krishna","Sam"]
+
+// // arr.splice(3,0,"Siva","Rama","Pavan")
+
+// // console.log(arr)
+
+// arr.splice(3,2,"Siva","Rama","Pavan")
+
+// console.log(arr)
+
+
+// map filter reduce
